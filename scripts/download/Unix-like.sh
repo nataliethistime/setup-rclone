6fe8c877350506a1e9e54b3e8fd15cc3d0a938a2
Natalie Rose <natalie@omg.lol>
2026-09-07 14:24:34 +1000

    Adding debugging

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -3,6 +3,7 @@ set -eu
 echo ::group::Downloading Rclone $version for $RUNNER_OS $RUNNER_ARCH
 if [ $RUNNER_OS = macOS ]; then os=osx; else os=linux; fi
 if [ $RUNNER_ARCH = ARM64 ]; then arch=arm64; else arch=amd64; fi
+echo "Downloading release $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-$os-$arch.zip";
 wget -q $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-$os-$arch.zip
 unzip -jq rclone-$version-$os-$arch.zip */rclone -d Rclone
 rm rclone-$version-$os-$arch.zip
